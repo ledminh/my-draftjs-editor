a0dfb9fbc6c03dca5f0eabf7bcd5adbd1de36260
Minh Le <duyminh.le310@gmail.com>
2022-01-27 01:12:18 -0500

    Setup primary editor

--- src/components/EditorComponent/index.js
+++ src/components/EditorComponent/index.js
@@ -1,10 +1,17 @@
 // import * as Styles from './styles';
 // import * as Const from './constants';
 
+import {Editor, EditorState} from 'draft-js';
+import 'draft-js/dist/Draft.css';
+import { useState } from 'react';
+
+
 const EditorComponent = () => {
+    const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
 
     return (
-        <></>
+        <Editor editorState={editorState} onChange={setEditorState} />
     )
 }
 
